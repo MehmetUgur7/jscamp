f0799af644b02fa3b3641cc71162d00ceb4b6917
MehmetUgur7 <77232030+MehmetUgur7@users.noreply.github.com>
2021-11-22 23:42:08 +0300

    22 Kasım ders sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -13,12 +14,26 @@ let user2 = new User(2,"Mehmet","Uğur","Antalya")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
-userService.getById(1)
-userService.list()
+//userService.getById(1)
+//userService.list()
 
 let customer = {id:1, firstName:"Engin"}
 
 customer.lastName="Demiroğ"
+
+console.log(customer.lastName)
+
+console.log(".............................")
+userService.load()
+
+let customerToAdd = new Customer(6,"Seda","Yılmaz","Ankara","şsdlkfjh")
+customerToAdd.type = "customer"
+
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
